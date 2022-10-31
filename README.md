b6f984188028afd9b4dac2dc7708372bd5fa1ba9
EduardNeto <86380315+EduardNeto@users.noreply.github.com>
2022-10-31 11:24:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# API_Git
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-qrrelz)
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const enviroment = {
+  urlBase: 'http://api.github.com',
+  production: false,
+};
